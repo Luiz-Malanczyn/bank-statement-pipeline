0d235be2bf93f375040b3303533b36c7e76022c7
Luiz Eduardo Malanczyn de Oliveira <oliveiraluiz.com@hotmail.com>
2025-04-12 16:43:27 -0300

    Implement PDF to DataFrame transformation and update pipeline to utilize new class

--- bank_statement_pipeline/pipeline/pipeline.py
+++ bank_statement_pipeline/pipeline/pipeline.py
@@ -1,5 +1,10 @@
 from bank_statement_pipeline.pipeline.extract.gmail_extractor import GmailExtractor
+from bank_statement_pipeline.pipeline.transform.pdf_to_dataframe import PDFToDataFrame
 
 if __name__ == '__main__':
-    extractor = GmailExtractor()
-    arquivos_salvos = extractor.download_pdf_attachments()
+    # extractor = GmailExtractor()
+    # arquivos_salvos = extractor.download_pdf_attachments()
+    transformer = PDFToDataFrame("data/bronze/gmail_attachments/Nubank_2025-04-02.pdf")
+    tables = transformer.extract_table()
+    transactions_df = transformer.convert_to_dataframe(tables)
+    print(transactions_df)
---
+++ bank_statement_pipeline/pipeline/transform/pdf_to_dataframe.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import pdfplumber
+from pathlib import Path
+import logging
+import re
+
+logging.getLogger("pdfminer").setLevel(logging.ERROR)
+
+class PDFToDataFrame:
+    def __init__(self, pdf_path: str):
+        self.pdf_path = Path(pdf_path)
+        if not self.pdf_path.exists():
+            raise FileNotFoundError(f"Arquivo PDF não encontrado: {self.pdf_path}")
+
+    def extract_table(self):
+        regex_pattern = r"TRANSAÇÕES DE \d{2} \w{3} A \d{2} \w{3}"
+        transaction_tables = []
+
+        with pdfplumber.open(self.pdf_path) as pdf:
+            for page in pdf.pages:
+                text = page.extract_text() or ""
+                lines = [line.strip() for line in text.split("\n")]
+
+                if any(re.search(regex_pattern, line) for line in lines):
+                    transaction_tables.append(page.extract_tables())
+
+            return transaction_tables
+        return None
+    
+    def convert_to_dataframe(self, tables):
+        rows = []
+        regex_with_parcela = r"(\d{2}) (\w{3}) (.+?) - (Parcela \d+/\d+) (R\$|US\$|€) (\d+,\d{2})"
+        regex_without_parcela = r"(\d{2}) (\w{3}) (.+?) (R\$|US\$|€) (\d+,\d{2})"
+
+        entries = [entry[0] for group in tables for item in group for entry in item]
+
+        for entry in entries:
+            match = re.match(regex_with_parcela, entry)
+            if match:
+                day, month, description, payment, coin, value = match.groups()
+            else:
+                match = re.match(regex_without_parcela, entry)
+                if match:
+                    day, month, description, coin, value = match.groups()
+                    payment = "In cash"
+                else:
+                    continue  # ignora se não casar com nenhum dos dois padrões
+
+            amount = float(value.replace(",", "."))
+            rows.append([day, month, description, payment, coin, amount])
+
+        df = pd.DataFrame(rows, columns=["day", "month", "description", "payment", "coin", "amount"])
+        return df
